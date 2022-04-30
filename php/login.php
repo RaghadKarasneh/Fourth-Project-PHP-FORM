36707f5562881339865f7d9d34eca7a340f1eee4
Raghad Karasneh <raghad.karasneh@gmail.com>
2022-05-01 01:31:57 +0300

    Fourth Project- PHP Form

--- php/login.php
+++ php/login.php
@@ -1,14 +1,42 @@
 <?php
 session_start();
-$_SESSION['loginEmail']=$_POST['loginEmail'];
-$_SESSION['loginPassword']=$_POST['loginPassword'];
-if ($_POST['submit']){
-    if($_SESSION['loginEmail']== $_SESSION['array'['Email']]){
-        echo 'correct';
-     }
-     else{
-         echo 'Incorrect';
-     }
+
+
+echo $_SESSION['email'];
+if (isset($_POST['submit'])){
+    
+    $_SESSION['loginEmail']=$_POST['loginEmail'];
+    $_SESSION['loginPassword']=$_POST['loginPassword'];
+
+    foreach ($_SESSION['array'] as $key => $value) {
+        //Check Email
+        if($key == 'Email'){
+            if($_SESSION['loginEmail']==$value){
+                $loginEmail_result="<span style=' color:green'>Correct Email</span>";
+                $loginEmail_correct=true;
+            }else{
+                $loginEmail_result="<span style=' color:red'>Incorrect Email</span>";
+                $loginEmail_correct=false;
+            }
+        }
+        //Check Password
+        if($key == 'Password Confirmation'){
+            if($_SESSION['loginPassword']==$value){
+                $loginPassword_result="<span style=' color:green'>Correct Password</span>";
+                $loginPassword_correct=true;
+            }else{
+                $loginPassword_result="<span style=' color:red'>Incorrect Password</span>";
+                $loginPassword_correct=false;
+            }
+        }
+    }
+    if($loginEmail_correct && $loginPassword_correct)
+        header('location:welcome.php');
+    else
+    echo '<script language="javascript">';
+    echo 'alert("Incorrect Information")';  //not showing an alert box.
+    echo '</script>';
+  
 }
 
 ?>
@@ -23,16 +51,20 @@ if ($_POST['submit']){
 </head>
 <body>
     <div class="container">
-        <form>
+        <form method="post">
             <h1>Login</h1>
             <p>Welcome back! Login with your credentials</p>
             <label for="loginEmail">Email</label>
             <br>
             <input type="email" name="loginEmail" id="loginEmail">
+            <?php if(isset($loginEmail_result)){echo $loginEmail_result;}?>
+            <br>
             
             <label for="loginPassword">Password</label>
             <br>
             <input type="password" name="loginPassword" id="loginPassword">
+            <?php if(isset($loginPassword_result)){echo $loginPassword_result;}?>
+            
             <br>
             <input type="submit" value="submit" name="submit">
             <!-- <div class="row">
--- php/welcome.php
+++ php/welcome.php
@@ -1,3 +1,4 @@
-<html>
-    <p>Welcome</p>
-</html>
+<?php
+session_start();
+echo "<p>Welcome ". $_SESSION['email'] ."your name is: ".$_SESSION['firstName']." ".$_SESSION['middleName']." ".$_SESSION['lastName']." ".$_SESSION['familyName']. " And your number is ". $_SESSION['phoneNumber']."</p>";
+?>
